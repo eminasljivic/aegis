ab8c88b199cb83a21a60c7952bdd74ef9907717b
Emina Sljivic <emina@sljivic.at>
2025-11-23 02:29:33 +0100

    Merge

--- composeApp/src/iosMain/kotlin/at/sljivic/aegis/MainViewController.kt
+++
@@ -1,5 +0,0 @@
-package at.sljivic.aegis
-
-import androidx.compose.ui.window.ComposeUIViewController
-
-fun MainViewController() = ComposeUIViewController { App() }
--- composeApp/src/iosMain/kotlin/at/sljivic/aegis/Platform.ios.kt
+++
@@ -1,9 +0,0 @@
-package at.sljivic.aegis
-
-import platform.UIKit.UIDevice
-
-class IOSPlatform : Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-}
-
-actual fun getPlatform(): Platform = IOSPlatform()
